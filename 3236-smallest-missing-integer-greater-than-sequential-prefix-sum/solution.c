b1e8f347b064d3f8bd3f3f3a2c17926c1237c2c1
BANDILA SIVA SRINU <236759359+SIVASRINUBANDILA@users.noreply.github.com>
2026-08-11 16:00:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (64.71%)

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/solution.c
@@ -0,0 +1,26 @@
+int missingInteger(int* nums, int numsSize) {
+    int total = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] == nums[i - 1] + 1) {
+            total += nums[i];
+        } else {
+            break;
+        }
+    }
+
+    bool found = true;
+
+    while (found) {
+        found = false;
+        for (int i = 0; i < numsSize; i++) {
+            if (nums[i] == total) {
+                found = true;
+                total += 1;
+                break;
+            }
+        }
+    }
+
+    return total;
+}
